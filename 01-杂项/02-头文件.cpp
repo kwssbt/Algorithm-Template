4cc2f04d894dc384174e278accd41980bafa4dda
Zhang <3393019220@qq.com>
2026-03-28 09:14:52 +0800

    头文件

--- 01-杂项/02-头文件.cpp
+++ 01-杂项/02-头文件.cpp
@@ -1,22 +1,15 @@
 #include<bits/stdc++.h>
-#include<ranges>
 using namespace std;
 
 typedef long long ll;
-typedef unsigned long long ull;
-typedef __int128 i128;
 
 void solve(){
 
 }
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
     int _=1;
-
     //cin>>_;
-    
-    cout<<fixed<<setprecision(15);
     while(_--)solve();
 }
